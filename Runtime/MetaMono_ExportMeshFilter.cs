6d48c202f63d286108783561c81011fd06484782
Eloi Stree <eloistree@gmail.com>
2025-03-31 00:18:51 +0200

    Save: 2025_03_31_00_18

--- Runtime/MetaMono_ExportMeshFilter.cs
+++ Runtime/MetaMono_ExportMeshFilter.cs
@@ -15,22 +15,27 @@ public class MetaMono_ExportMeshFilter : MonoBehaviour
     public bool m_allExportOnEnable = false;
     public bool m_allExportOnDisable = false;
 
+    public void SetWithMeshFilter(MeshFilter meshFilter)
+    {
+        m_meshFilter = meshFilter;
+    }
     private void OnEnable()
     {
         if (m_allExportOnEnable)
-            SaveObj();
+            ExportOBJ();
     }
 
     private void OnDisable()
     {
         if (m_allExportOnDisable)
-            SaveObj();
+            ExportOBJ();
     }
 
+    public bool m_useSharedMesh = true;
     [ContextMenu("Save persistent OBJ")]
-    public void SaveObj()
+    public void ExportOBJ()
     {
-        Mesh mesh = m_meshFilter.sharedMesh;
+        Mesh mesh =m_useSharedMesh?  m_meshFilter.sharedMesh: m_meshFilter.mesh;
         if (mesh == null)
             return;
         if (m_whereToExport == null)
